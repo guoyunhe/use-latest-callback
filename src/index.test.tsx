3ad4b4e4709a02acd1946ed4f2d416c7f67b731f
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2025-01-13 14:42:38 +0800

    test

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,9 +1,28 @@
 import { render, screen } from '@testing-library/react';
-import { UseLatestCallback } from '.';
+import { useState } from 'react';
+import { useLatestCallback } from '.';
 
-describe('UseLatestCallback', () => {
-  it('render', async () => {
-    render(<UseLatestCallback>foobar</UseLatestCallback>);
-    await screen.findByText('foobar');
+describe('useLatestCallback()', () => {
+  it('should keep reference', async () => {
+    let callback = null;
+    const App = () => {
+      const [foobar, setFoobar] = useState('foo');
+
+      callback = useLatestCallback(() => foobar);
+
+      return <button onClick={() => setFoobar('bar')}>{foobar}</button>;
+    };
+    render(<App />);
+
+    expect(callback()).toBe('foo');
+    const oldCallback = callback;
+
+    screen.getByText('foo').click();
+    await screen.findByText('bar');
+
+    // callback can get latest state value
+    expect(callback()).toBe('bar');
+    // callback reference didn't change
+    expect(callback).toBe(oldCallback);
   });
 });
